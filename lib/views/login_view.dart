6de0d751dbb2482e57927ba6b950439c62ea6a55
UrsaMedium <celestial.lovelace@gmail.com>
2024-01-10 22:08:06 +0300

    step-7

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -67,10 +67,18 @@ class _LoginViewState extends State<LoginView> {
                       email: email,
                       password: password
                   );
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route) => false,
-                  );
+                  final user = FirebaseAuth.instance.currentUser;
+                  if (user?.emailVerified ?? false) {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      notesRoute,
+                      (route) => false,
+                    );
+                  } else {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute,
+                      (route) => false,
+                    );
+                  }                 
                 } on FirebaseAuthException catch (e) {
                   devtools.log(e.code.toString());
                   await showErrorDialog(context, e.code.toString());
