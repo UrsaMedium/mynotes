10579f03e21d0103e90c8e27debb3ac3639b531d
UrsaMedium <celestial.lovelace@gmail.com>
2024-01-20 13:23:20 +0300

    step-11

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:mynotes/constants/toutes.dart';
-import 'package:mynotes/services/crud/crud_crudexceptions.dart';
+import 'package:mynotes/services/crud/crud_exceptions.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' show join;
